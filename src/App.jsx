bc13314f7a6fa077652021b0eee3545c4f81c7e6
RomwelFeliciano <romfeliciano18@gmail.com>
2023-11-02 12:17:17 +0800

    Set up and Nav Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  let Links = [
+    { name: "Home", link: "#Home" },
+    { name: "Skills", link: "#Skills" },
+    { name: "Certifications", link: "#Certifications" },
+    { name: "Experience", link: "#Experience" },
+    { name: "Projects", link: "#Projects" },
+    { name: "Contact", link: "#Contact" },
+  ];
+
+  let [open, setOpen] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <header className="shadow-md w-full fixed top-0 left-0 z-10">
+      <nav className="md:flex items-center justify-between bg-white py-4 md:px-20 px-7 transition-all duration-500 ease-in">
+        <div className="font-bold text-2xl cursor-pointer flex justify-between items-center">
+          <a href="#Home">
+            {/* <img className="w-14" src="logo.png" alt="" /> */}
+            <button className="">Logo</button>
+          </a>
 
-export default App
+          <div
+            className="text-3xl right-8 top-6 cursor-pointer md:hidden transition-all duration-300"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <AiOutlineClose /> : <HiOutlineMenuAlt3 />}
+          </div>
+        </div>
+        <ul
+          className={`md:flex gap-5 md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-16 " : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li key={link.name} className="text-xl md:my-0 my-7">
+              <a
+                href={link.link}
+                className="text-gray-800 hover:text-gray-400 duration-500"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+          <div className="flex md:hidden">
+            <button className="">Dark Mode</button>
+          </div>
+        </ul>
+        <div className="hidden md:flex">
+          <button className="">Dark Mode</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        main: "#6DD9D2",
+        second: "#393E46",
+        body: "#EEEEEE",
+        accent: "#1D1D1D",
+        night: "#f0f0f0",
+      },
+    },
+  },
+  plugins: [],
+};
