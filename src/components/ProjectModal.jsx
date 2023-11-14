cb6e7e8ff785fb584e076454d43d22cfeac82314
Romwel Feliciano <127919521+RomwelFeliciano@users.noreply.github.com>
2023-11-14 14:28:01 +0800

    Updated

---
+++ src/components/ProjectModal.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProjectModal = ({ projects, activeID, setShowModal }) => {
+  const project = projects.find((proj) => proj.id === activeID);
+
+  return <div>asd</div>;
+};
+
+export default ProjectModal;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,56 +1,66 @@
 import { useState } from "react";
+import ProjectModal from "./ProjectModal";
 
 const Projects = () => {
   const projects = [
     {
+      id: 1,
       name: "Kloudtech",
       image: "images/Projects/kloudtech.png",
       details:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ullam sequi culpa quisquam esse aperiam veritatis atque, perferendis quidem sit id laboriosam soluta unde reprehenderit nobis incidunt voluptas voluptatem! Pariatur.",
     },
     {
+      id: 2,
       name: "Kloudtrack",
       image: "images/Projects/kloudtrack.png",
       details:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ullam sequi culpa quisquam esse aperiam veritatis atque, perferendis quidem sit id laboriosam soluta unde reprehenderit nobis incidunt voluptas voluptatem! Pariatur.",
     },
     {
+      id: 3,
       name: "Pizza Menu",
       image: "images/Projects/pizzamenu.png",
       details:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ullam sequi culpa quisquam esse aperiam veritatis atque, perferendis quidem sit id laboriosam soluta unde reprehenderit nobis incidunt voluptas voluptatem! Pariatur.",
     },
     {
+      id: 4,
       name: "Pic-Sell Planet",
       image: "images/Projects/picsellplanet.png",
       details:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ullam sequi culpa quisquam esse aperiam veritatis atque, perferendis quidem sit id laboriosam soluta unde reprehenderit nobis incidunt voluptas voluptatem! Pariatur.",
     },
     {
+      id: 5,
       name: "Travel List",
       image: "images/Projects/travellist.png",
       details:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ullam sequi culpa quisquam esse aperiam veritatis atque, perferendis quidem sit id laboriosam soluta unde reprehenderit nobis incidunt voluptas voluptatem! Pariatur.",
     },
     {
+      id: 6,
       name: "Grocery Buddy",
       image: "images/Projects/grocery.png",
       details:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ullam sequi culpa quisquam esse aperiam veritatis atque, perferendis quidem sit id laboriosam soluta unde reprehenderit nobis incidunt voluptas voluptatem! Pariatur.",
     },
     {
+      id: 7,
       name: "Eat and Split",
       image: "images/Projects/eatnsplit.png",
       details:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ullam sequi culpa quisquam esse aperiam veritatis atque, perferendis quidem sit id laboriosam soluta unde reprehenderit nobis incidunt voluptas voluptatem! Pariatur.",
     },
     {
+      id: 8,
       name: "Guess The Number",
       image: "images/Projects/guess.png",
       details:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ullam sequi culpa quisquam esse aperiam veritatis atque, perferendis quidem sit id laboriosam soluta unde reprehenderit nobis incidunt voluptas voluptatem! Pariatur.",
     },
     {
+      id: 9,
       name: "Student Information",
       image: "images/Projects/student.png",
       details:
@@ -60,9 +70,16 @@ const Projects = () => {
 
   const [nextItems, setNextItems] = useState(6);
   const [portfolios, setPortfolios] = useState(projects);
+  const [showModal, setShowModal] = useState(false);
+  const [activeID, setActiveID] = useState(null);
 
   const loadMoreHandler = () => {
     setNextItems((prev) => prev + 3);
+  };
+
+  const showModalHandler = (id) => {
+    setShowModal(true);
+    setActiveID(id);
   };
 
   return (
@@ -74,20 +91,23 @@ const Projects = () => {
         Projects
       </h2>
       <div className="flex items-center gap-4 flex-wrap">
-        {portfolios.slice(0, nextItems)?.map((proj, index) => (
+        {portfolios.slice(0, nextItems)?.map((proj) => (
           <div
             //   data-aos="fade-zoom-in"
             //   data-aos-delay="50"
             //   data-aos-duration="1000"
             className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg:w-[32.2%] relative z-[1]"
-            key={index}
+            key={proj.id}
           >
             <figure>
               <img className="rounded-[8px]" src={proj.image} alt={proj.name} />
             </figure>
             <div className="w-full h-full bg-main bg-opacity-0 absolute top-0 left-0 z[5] group-hover:bg-opacity-40 duration-300 ease-in-out rounded-[8px]">
               <div className="w-ful h-full flex items-center justify-center">
-                <button className="text-transparent bg-opacity-0 group-hover:text-white group-hover:bg-main duration-300 ease-in-out py-2 px-4 rounded-[8px]">
+                <button
+                  onClick={() => showModalHandler(projects.id)}
+                  className="text-transparent bg-opacity-0 group-hover:text-white group-hover:bg-main duration-300 ease-in-out py-2 px-4 rounded-[8px]"
+                >
                   See Details
                 </button>
               </div>
@@ -107,6 +127,8 @@ const Projects = () => {
           </button>
         </div>
       )}
+
+      {showModal && <ProjectModal setShowModal={setShowModal} activeID={activeID} projects={projects} />}
     </section>
   );
 };
